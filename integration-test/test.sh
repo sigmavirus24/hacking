40308d721470ee1d759bab63b9b9616e4557f044
Andreas Jaeger <aj@suse.com>
2016-01-15 19:22:21 +0100

    Avoid git clone in integration test
    Allow script to be called with a third argument which is a path to a
    checked out directory. This allows setting up the repo externally using
    zuul-cloner in the infra script and allows usage of depends-on.
    
    Change-Id: I3b6180b6b274966de63fe281021ff9f4186a929b
    Needed-By: I828c1875e5f008fa55b693ad210421794395f623

--- integration-test/test.sh
+++ integration-test/test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Usage: test.sh openstack keystone
+# Usage: test.sh openstack keystone path-to-repo
+# path-to-repo is an optional parameter, if it exists
+# no cloning will happen and the local directory will be used,
+# the first two parameter get ignored.
 # Note: you can clone from a local file with REPO_ROOT=file:////~/path/to/repo
+
 set -x
 set -e
 
 REPO_ROOT=${REPO_ROOT:-git://git.openstack.org}
+
 if [[ -z "$2" ]]; then
     org=openstack
     project=nova
@@ -13,20 +18,32 @@ else
     org=$1
     project=$2
 fi
+if [[ $# -eq 3 ]] ; then
+    projectdir=$3
+    clone=0
+else
+    clone=1
+    projectdir=$project
+fi
 
-tempdir="$(mktemp -d)"
+if [ "$clone" = "1" ] ; then
 
-pushd $tempdir
+    tempdir="$(mktemp -d)"
+
+    trap "rm -rf $tempdir" EXIT
+    pushd $tempdir
     if [[ $REPO_ROOT  == file://* ]]; then
         git clone $REPO_ROOT/$org/$project
     else
         git clone $REPO_ROOT/$org/$project --depth=1
     fi
+fi
 
-    pushd $project
-        set +e
-        flake8 --select H --statistics
-    popd
+pushd $projectdir
+set +e
+flake8 --select H --statistics
 popd
 
-rm -rf $tempdir
+if [ "$clone" = "1" ] ; then
+    popd
+fi
